c6070ac19ddcc7980e7a59b3ef85643386d1c7d9
alanctprado <alanctprado@gmail.com>
2024-04-14 12:36:22 -0300

    Added graph.cpp file description

--- src/graph.cpp
+++ src/graph.cpp
@@ -1,3 +1,18 @@
+/******************************************************************************
+ * Top contributors (to current version):
+ *   Alan Prado, Kaio Vieira, Laila Melo, Guilherme Gomes
+ *
+ * This file is part of Banana, a one-sided crossing minimization solver.
+ *
+ * Copyright (c) 2024 by the authors listed in the file AUTHORS in the
+ * top-level source directory and their institutional affiliations. All rights
+ * reserved. See the file LICENSE.md in the top-level source directory for
+ * licensing information.
+ * ****************************************************************************
+ *
+ * Basic graph class.
+ */
+
 #include "graph.h"
 
 #include <algorithm>
