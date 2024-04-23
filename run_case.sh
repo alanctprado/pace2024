50f7963c340218234b093c82b40945d2391c8fd7
luishgh <51053603+luishgh@users.noreply.github.com>
2024-04-22 22:24:37 -0300

    Implement verifySolution and update run_case.sh
    Implement command line option that verifies if the number of crossings
    computed is the same of a given solution file. Update run_case.sh to
    use it.

--- run_case.sh
+++ run_case.sh
@@ -5,11 +5,6 @@ if [ $# -ne 2 ]; then
     exit 1
 fi
 
-mkdir -p /tmp/pace
-
 # Run test case, the solution is expected to be verified inside the solver itself
 echo "running for case $1/$2:"
-./build/pace < "./test/$1/instances/$2.gr" > "/tmp/pace/$2.out"
-rm "/tmp/pace/$2.out"
-
-rmdir /tmp/pace
+./build/pace --verify="./test/$1/solutions/$2.sol" < "./test/$1/instances/$2.gr" > /dev/null
