09f562ca797104ff64e313d25de30d189e8eb3d8
Bananalyzer <bananalyzer@pace.com>
2024-05-01 03:55:35 +0000

    Automated changes

--- src/barycenter_heuristic.cpp
+++ src/barycenter_heuristic.cpp
@@ -16,43 +16,49 @@
 #include "barycenter_heuristic.h"
 #include <algorithm>
 
-namespace banana{
-namespace solver{
-namespace heuristic{
-namespace barycenter{
-  
+namespace banana {
+namespace solver {
+namespace heuristic {
+namespace barycenter {
+
 BarycenterHeuristic::BarycenterHeuristic(graph::BipartiteGraph graph)
     : ApproximationRoutine(graph)
 {
   int n = graph.countVerticesB();
   int offset = graph.countVerticesA();
-  
-  for (int i = 0; i < n; ++i){
-    m_neighborhoodInfo.push_back(getNeighborhoodInfo(i+offset));
+
+  for (int i = 0; i < n; ++i)
+  {
+    m_neighborhoodInfo.push_back(getNeighborhoodInfo(i + offset));
   }
 }
 
 /**
  * Sorts vertices in layer B by the mean of their neighbors.
- * 
- * To avoid working with non-integer values, the corresponding mean for each node is
- * represented as a fraction by the pair (sum of neighbors, number of neighbors)
+ *
+ * To avoid working with non-integer values, the corresponding mean for each
+ * node is represented as a fraction by the pair (sum of neighbors, number of
+ * neighbors)
  */
 int BarycenterHeuristic::solve()
 {
   int n = m_graph.countVerticesB();
   int offset = m_graph.countVerticesA();
-  
+
   std::vector<int> b_layer;
-  for (int i = 0; i < n; ++i) b_layer.push_back(i);
+  for (int i = 0; i < n; ++i)
+    b_layer.push_back(i);
 
   std::sort(b_layer.begin(), b_layer.end(), [&](int node1, int node2) {
-        long long v1 = (1ll * m_neighborhoodInfo[node1].first * m_neighborhoodInfo[node2].second);
-        long long v2 = (1ll * m_neighborhoodInfo[node2].first * m_neighborhoodInfo[node1].second);
-        return (v1 < v2);
+    long long v1 = (1ll * m_neighborhoodInfo[node1].first *
+                    m_neighborhoodInfo[node2].second);
+    long long v2 = (1ll * m_neighborhoodInfo[node2].first *
+                    m_neighborhoodInfo[node1].second);
+    return (v1 < v2);
   });
-    
-  for (int i = 0; i < n; ++i) b_layer[i] += offset;
+
+  for (int i = 0; i < n; ++i)
+    b_layer[i] += offset;
 
   return numberOfCrossings(b_layer);
 }
@@ -60,12 +66,13 @@ int BarycenterHeuristic::solve()
 /**
  * Calculates the pair (sum of neighbors, number of neighbors) for a given node
  */
-std::pair<int,int> BarycenterHeuristic::getNeighborhoodInfo(int node)
+std::pair<int, int> BarycenterHeuristic::getNeighborhoodInfo(int node)
 {
   std::vector<int> neighbors = m_graph.neighborhood(node);
 
   int neighborhood_sum = 0;
-  for (auto v : neighbors) neighborhood_sum += v;
+  for (auto v : neighbors)
+    neighborhood_sum += v;
 
   int neighbordhood_size = neighbors.size();
 
--- src/barycenter_heuristic.h
+++ src/barycenter_heuristic.h
@@ -20,27 +20,31 @@
 #include <vector>
 #include <utility>
 
-namespace banana{
-namespace solver{
-namespace heuristic{
-namespace barycenter{
+namespace banana {
+namespace solver {
+namespace heuristic {
+namespace barycenter {
 
 /**
  * Implements the Barycenter Heuristic
- * 
- * In short, the algorithm orders the vertices on layer B by the mean of their neighbors. If two vertices
- * share the same mean, it preserves their original arrangement. This can be done in polynomial time
- * BIGO( max(M, |B|log|B|) ), where M is the number of edges in the graph.
- * 
- * The Barycenter Heuristic was the first popular approximation algorithm developed for the OSCM problem,
- * credited to Sugiyama & Tagawa (1981). Its theorical bounds and performance are well documented in the
- * literature, e.g. Makinen(1990), Eades & Wormald (1994), Junger & Mutzel (1997), Li & Stallmann (2001).
- * 
- * The approximation ratio is BIGTHETA(min(sqrt(n), n/d)), where d is the minimum degree of any vertex,
- * or BIGO(D-1), where D is the maximum degree of a vertex in the permutable layer.
- * 
- * In practice, it boasts decent results on sparse graphs, whereas other heuristics are either too inaccurate
- * or too slow.
+ *
+ * In short, the algorithm orders the vertices on layer B by the mean of their
+ * neighbors. If two vertices share the same mean, it preserves their original
+ * arrangement. This can be done in polynomial time BIGO( max(M, |B|log|B|) ),
+ * where M is the number of edges in the graph.
+ *
+ * The Barycenter Heuristic was the first popular approximation algorithm
+ * developed for the OSCM problem, credited to Sugiyama & Tagawa (1981). Its
+ * theorical bounds and performance are well documented in the literature, e.g.
+ * Makinen(1990), Eades & Wormald (1994), Junger & Mutzel (1997), Li & Stallmann
+ * (2001).
+ *
+ * The approximation ratio is BIGTHETA(min(sqrt(n), n/d)), where d is the
+ * minimum degree of any vertex, or BIGO(D-1), where D is the maximum degree of
+ * a vertex in the permutable layer.
+ *
+ * In practice, it boasts decent results on sparse graphs, whereas other
+ * heuristics are either too inaccurate or too slow.
  */
 class BarycenterHeuristic : public ApproximationRoutine
 {
@@ -50,16 +54,13 @@ public:
   int solve() override;
 
 private:
-  std::vector<std::pair<int,int>> m_neighborhoodInfo;
-  std::pair<int,int> getNeighborhoodInfo(int node);
-}; 
+  std::vector<std::pair<int, int>> m_neighborhoodInfo;
+  std::pair<int, int> getNeighborhoodInfo(int node);
+};
 
 } // namespace barycenter
 } // namespace heuristic
 } // namespace solver
 } // namespace banana
 
-
 #endif // __PACE_2024__BARYCENTER_HEURISTIC_H
-
-
