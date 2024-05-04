191f43a781e5a719212ee4f49dacb3310802b98f
alanctprado <alanctprado@gmail.com>
2024-05-04 20:28:30 -0300

    Initial Gurobi formulation

---
+++ src/ip_solver_gurobi.h
@@ -0,0 +1,49 @@
+/******************************************************************************
+ * Top contributors (to current version):
+ *   Alan Prado, Kaio Vieira
+ *
+ * This file is part of Banana, a one-sided crossing minimization solver.
+ *
+ * Copyright (c) 2024 by the authors listed in the file AUTHORS in the
+ * top-level source directory and their institutional affiliations. All rights
+ * reserved. See the file LICENSE.md in the top-level source directory for
+ * licensing information.
+ * ****************************************************************************
+ *
+ * Gurobi extension of the integer programming solver
+ */
+
+#ifndef __PACE2024__GUROBI_SOLVER_HPP
+#define __PACE2024__GUROBI_SOLVER_HPP
+
+#include "bipartite_graph.h"
+#include "ip_solver.h"
+#include "../gurobi/include/gurobi_c++.h"
+
+#include <vector>
+
+namespace banana {
+namespace solver {
+namespace ip {
+
+/** TODO: document */
+class GurobiSolver : public IntegerProgrammingSolver<GRBModel, GRBVar>
+{
+public:
+  GurobiSolver(graph::BipartiteGraph G);
+  ~GurobiSolver() = default;
+
+  int simple() override;
+  int shorter() override;
+  int quadratic() override;
+  int vini() override;
+
+  void xPrefix(GRBModel* model, GRBVar& vars) override;
+  void yPrefix(GRBModel* model, GRBVar& vars) override;
+};
+
+} // namespace ip
+} // namespace solver
+} // namespace banana
+
+#endif // __PACE2024__GUROBI_SOLVER_HPP
