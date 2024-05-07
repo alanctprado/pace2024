eed7b7d2bd218bc4d9b4e790f6c6d2b9ceffa4ca
Gabriel Vieira <gabrelvieira@outlook.com>
2024-05-06 20:40:35 -0400

    Median Heuristic

---
+++ src/median_heuristic.h
@@ -0,0 +1,48 @@
+/******************************************************************************
+ * Top contributors (to current version):
+ *   Gabriel Vieira
+ *
+ * This file is part of Banana, a one-sided crossing minimization solver.
+ *
+ * Copyright (c) 2024 by the authors listed in the file AUTHORS in the
+ * top-level source directory and their institutional affiliations. All rights
+ * reserved. See the file LICENSE.md in the top-level source directory for
+ * licensing information.
+ * ****************************************************************************
+ *
+ * Implementation of the Median Heuristic for OSCM
+ */
+
+#ifndef __PACE2024__MEDIAN_HEURISTIC_H
+#define __PACE2024__MEDIAN_HEURISTIC_H
+
+#include "approximation_routine.h"
+#include <vector>
+#include <utility>
+
+namespace banana {
+namespace solver {
+namespace heuristic {
+namespace median {
+
+/**
+ * Implements the Median Heuristic
+ */
+class MedianHeuristic : public ApproximationRoutine
+{
+public:
+  MedianHeuristic(graph::BipartiteGraph graph);
+  ~MedianHeuristic() override = default;
+  int solve() override;
+
+private:
+  std::vector<int>* couting;
+  int median(std::vector<int>& neighbors);
+};
+
+} // namespace median
+} // namespace heuristic
+} // namespace solver
+} // namespace banana
+
+#endif // __PACE_2024__MEDIAN_HEURISTIC_H
