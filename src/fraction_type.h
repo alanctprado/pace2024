b398ebac0794e82e5925a8a51be2d88dadb57709
Kaio Vieira <kaiomassevieira@gmail.com>
2024-05-21 09:01:43 -0300

    Update Fraction implementation, and getCrossings

--- src/fraction_type.h
+++ src/fraction_type.h
@@ -36,54 +36,67 @@ namespace library {
 template <class T> class Fraction
 {
 protected:
-  T num, den;
+  T _num, _den;
 
 public:
   Fraction(const T &_num=0, const T &_den=1);
+
+  const T& num() const;
+  const T& den() const;
 };
 
 template <class T>
-Fraction<T>::Fraction(const T &_num, const T &_den) : num(_num), den(_den)
+Fraction<T>::Fraction(const T &_num, const T &_den) : _num(_num), _den(_den)
 {
-  assert(den != 0);
-  if (den < 0)
+  assert(_den != 0);
+  if (_den < 0)
   {
-    num *= -1;
-    den *= -1;
+    _num *= -1;
+    _den *= -1;
   }
-  T g = gcd(num, den);
-  num /= g;
-  den /= g;
+  T g = gcd(_num, _den);
+  _num /= g;
+  _den /= g;
+}
+
+template <class T>
+const T& Fraction<T>::num() const {
+    return _num;
+}
+
+template <class T>
+const T& Fraction<T>::den() const {
+    return _den;
 }
 
 template <class T>
 inline bool operator <(const Fraction<T> &lhs, const Fraction<T> &rhs)
 {
-  return (lhs.num * rhs.den < lhs.den * rhs.num);
+  return (lhs._num * rhs._den < lhs._den * rhs._num);
 }
 
 template <class T>
 inline Fraction<T> operator +(const Fraction<T> &lhs, const Fraction<T> &rhs)
 {
-  return {lhs.num * rhs.den + lhs.den * rhs.num, lhs.den * rhs.den};
+  return {lhs._num * rhs._den + lhs._den * rhs._num, lhs._den * rhs._den};
 }
 
 template <class T>
 inline Fraction<T> operator -(const Fraction<T> &lhs, const Fraction<T> &rhs)
 {
-  return {lhs.num * rhs.den - lhs.den * rhs.num, lhs.den * rhs.den};
+  return {lhs._num * rhs._den - lhs._den * rhs._num, lhs._den * rhs._den};
 }
 
 template <class T>
 inline Fraction<T> operator *(const Fraction<T> &lhs, const Fraction<T> &rhs)
 {
-  return {lhs.num * rhs.num, lhs.den * rhs.den};
+  return {lhs._num * rhs._num, lhs._den * rhs._den};
 }
 
 template <class T>
 inline Fraction<T> operator /(const Fraction<T> &lhs, const Fraction<T> &rhs)
 {
-  return {rhs.num * rhs.den, lhs.den * rhs.num};
+  return {rhs._num * rhs._den, lhs._den * rhs._num};
 }
 
 } // namespace library
